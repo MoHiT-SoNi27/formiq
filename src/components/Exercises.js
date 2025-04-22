4ac8fc77af513fcef7f9aa65262ce81d6bed9d79
MOHIT SONI <mohitsoni25017@gmail.com>
2025-04-22 17:47:55 +0530

    Add BodyPart & ExerciseCard and update remaining

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import { exerciseOptions, fetchData } from '../utils/fetchData'
 
 const Exercises = () => {
   return (
-    <div>
-      Exercises
-    </div>
+    <Box id="exercises" sx={{ mt: { lg: '110px', xs: '70px' } }} p="20px">  
+      <Typography variant="h3" mb="46px">
+        Exercises
+      </Typography>
+      <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+        <Box sx={{ flexBasis: '100%', textAlign: 'center' }}>
+          <Typography variant="h5" mb="25px">
+            Showing Results
+          </Typography>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,5 @@
 export const exerciseOptions = {
     method: 'GET',
-    url: 'https://exercisedb.p.rapidapi.com/status',
     headers: {
       'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
       'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
